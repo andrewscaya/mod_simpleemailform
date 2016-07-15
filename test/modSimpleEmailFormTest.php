cd422a9542fe38915abab213ba77fe0e419c86f9
martinbeaudry <martinbeaudry@hotmail.com>
2016-07-14 22:43:31 -0400

    2016-07-14 MB: Adds unit tests for the formatErrorMessage() method.

--- test/modSimpleEmailFormTest.php
+++ test/modSimpleEmailFormTest.php
@@ -113,6 +113,66 @@ class modSimpleEmailFormTest extends PHPUnit_Framework_TestCase
 //     }
 
     /**
+    * formatErrorMessage
+    */
+    
+    //Argument for color
+	protected $color = "red";
+
+	//Arguments for message
+	protected $standardMessage = "This is a test";
+	protected $messageNull = null;
+	protected $emptyMessage = " ";
+	
+	//Arguments for filename
+	protected $fn = "test.php";
+	protected $fnNull = null;
+	protected $fnSpace = " ";
+
+	//Three tests to ckeck the function's behaviour with filenames
+	public function testFormatErrorMessageNoFn () {
+		$message = formatErrorMessage($this->color, $this->standardMessage);
+		$this->assertSame("<p><b><span style='color:$this->color;'>$this->standardMessage</span></b></p>\n", $message);
+	}
+
+	public function testFormatErrorMessageWithFn () {
+		$message = formatErrorMessage($this->color, $this->standardMessage, $this->fn);
+		$this->assertSame("<p><b><span style='color:$this->color;'>$this->standardMessage ($fn)</span></b></p>\n", $message);
+	}
+
+	public function testFormatErrorMessageInvalidFn () {
+		$message = formatErrorMessage($this->color, $this->standardMessage, $this->fnSpace);
+		$this->assertSame("<p><b><span style='color:$this->color;'>$this->standardMessage 
+			('Warning - Invalid filename: no alnum character')</span></b></p>\n", $message);
+	}
+
+	//Three tests to ckeck the function's behaviour with messages
+	public function testFormatErrorMessageNullMessage () {
+		$message = formatErrorMessage($this->color, $this->messageNull, $this->fn);
+		$this->assertSame("<p><b><span style='color:$this->color;'>$this->standardMessage 
+			('Warning - Invalid filename: no alnum character')</span></b></p>\n", $messagee);
+	}
+
+	public function testFormatErrorMessageSpaceMessage () {
+		$message = formatErrorMessage($this->color, $this->emptyMessage, $this->fn);
+		$this->assertSame("<p><b><span style='color:$this->color;'>$this->standardMessage 
+			('Warning - Invalid filename: no alnum character')</span></b></p>\n", $message);
+	}
+
+	public function testFormatErrorMessageRealMessage () {
+		$message = formatErrorMessage($this->color, $this->standardMessage, $this->fn);
+		$this->assertSame("<p><b><span style='color:$this->color;'>$this->standardMessage 
+			($this->fn)</span></b></p>\n", $message);
+	}
+
+	//Only important thing with color is that the value is put in its 
+	//rightful place. Html is valid no matter what the value is.
+	public function testFormatErrorMessageColor () {
+		$message = formatErrorMessage($this->color, $this->standardMessage, $this->fn);
+		$this->assertSame("<p><b><span style='color:$this->color;'>$this->standardMessage 
+			($this->fn)</span></b></p>\n", $message);
+    }
+    /**
      * Tests modSimpleEmailForm->main()
      */
     public function testMain()
