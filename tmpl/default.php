43e7cef25064087670c1b49ef43ff4f303b100fd
Andrew Caya <andrewscaya@yahoo.ca>
2017-12-21 22:09:57 -0500

    2017-12-21 AC: Fixes an issue concerning the full path of the custom view file.

--- tmpl/default.php
+++ tmpl/default.php
@@ -3,12 +3,12 @@ defined('_JEXEC') or die('Restricted access'); ?>
 
 <?php
 
-$defaultCustomFile = 'default_custom.php';
+$defaultCustomFile = __DIR__ . DIRECTORY_SEPARATOR . 'default_custom.php';
 
 if (!$form->getFormRendering() && file_exists($defaultCustomFile)) {
     $view = require_once "$defaultCustomFile";
 } elseif (!$form->getFormRendering() && !file_exists($defaultCustomFile)) {
-    $view = "Custom default view file /tmpl/$defaultCustomFile not found.";
+    $view = "Custom default view file $defaultCustomFile not found.";
 }
 
 echo $view;
