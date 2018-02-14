d28941e9d149dbd9672bf3ea28905feb3932530e
Andrew Caya <andrewscaya@yahoo.ca>
2018-02-13 19:51:56 -0500

    2018-02-13 AC: Re-enables functional tests.

---
+++ bin/selenium-setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+cd /tmp
+wget -O joomla_3-8-5-stable-full_package.zip https://downloads.joomla.org/cms/joomla3/3-8-5/Joomla_3-8-5-Stable-Full_Package.zip?format=zip
+git clone https://github.com/andrewscaya/mod_simpleemailform
+cd mod_simpleemailform
+git fetch --all
+git checkout stable
+cd /tmp
+mkdir joomla
+cd joomla
+unzip ../joomla_3-8-5-stable-full_package.zip
+(php -S localhost:8181 > /dev/null 2>&1) &
+exit 0
