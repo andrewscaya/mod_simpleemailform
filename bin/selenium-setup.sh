a67ff63e3b5a32e090b090ac1b547a02f84dab88
Andrew Caya <andrewscaya@yahoo.ca>
2018-02-13 21:23:50 -0500

    2018-02-13 AC: Fixes a functional test.

--- bin/selenium-setup.sh
+++ bin/selenium-setup.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 cd /tmp
 wget -O joomla_3-8-5-stable-full_package.zip https://downloads.joomla.org/cms/joomla3/3-8-5/Joomla_3-8-5-Stable-Full_Package.zip?format=zip
-git clone https://github.com/andrewscaya/mod_simpleemailform
-cd mod_simpleemailform
-git fetch --all
-git checkout stable
-cd /tmp
+wget -O mod_simpleemailform-latest.tar.gz https://codeload.github.com/andrewscaya/mod_simpleemailform/tar.gz/latest
 mkdir joomla
 cd joomla
 unzip ../joomla_3-8-5-stable-full_package.zip
